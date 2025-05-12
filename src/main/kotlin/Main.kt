ec620c27f680ffe88414d12e65ac94528891b75d
Ala Ashowiter <olalcosh@gmail.com>
2025-05-13 00:36:18 +0300

    rename GetRandomMealUseCase to GetGuessGameUseCase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,13 @@ package org.example
 
 import org.example.data.repository.data.repository.MockDataMealRepository
 import org.example.domain.usecase.GetEasyPreparedMealsUseCase
-import org.example.domain.usecase.GetRandomMealUseCase
+import org.example.domain.usecase.GetGuessGameUseCase
 
 fun main() {
     val mealsUseCase = GetEasyPreparedMealsUseCase(MockDataMealRepository())
     mealsUseCase.getEasyPreparedMeals().forEach {
         println(it.name)
     }
-    val randomMealUseCase= GetRandomMealUseCase(MockDataMealRepository())
+    val randomMealUseCase= GetGuessGameUseCase(MockDataMealRepository())
     println("${randomMealUseCase.GetRandomMeal().name}  ${randomMealUseCase.GetRandomMeal().minutes}")
 }
