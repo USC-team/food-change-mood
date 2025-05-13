e088a37df5d1a9334c3bb3935706a4dc40248343
Ahmed.A <ahmedalimohammedgebril@gmail.com>
2025-05-13 16:32:18 +0300

    Change repository

--- src/main/kotlin/domain/usecase/GetSpecialIraqMealsUseCae.kt
+++ src/main/kotlin/domain/usecase/GetSpecialIraqMealsUseCae.kt
@@ -1,9 +1,9 @@
 package org.example.domain.usecase
 
 import domain.model.Meal
-import org.example.data.repository.data.repository.MockDataMealRepository
+import org.example.domain.repository.domain.repository.MealsRepository
 
-class GetSpecialIraqMealsUseCae (private val repo: MockDataMealRepository){
+class GetSpecialIraqMealsUseCae (private val repo: MealsRepository){
     fun getSpecialIraqMeals(country : String):List<Meal>{
         val allMeals = repo.getAllMeals().filter { it.name != null || it.description !=null}
         val iraqMeals = allMeals.filter { it.name?.contains(country) == true || it.description?.contains(country) == true }
