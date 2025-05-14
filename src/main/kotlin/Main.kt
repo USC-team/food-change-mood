ff05add92e0e95b588b7eb87f304318b8926265d
Ala Ashowiter <olalcosh@gmail.com>
2025-05-14 23:42:31 +0300

    remove unused imports directives, deleted comments, and solved warnings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,6 @@
 package org.example
 import org.example.presentation.FoodChangeMoodConsole
-
-import org.example.data.repository.MockDataMealRepository
 import org.example.dependencyInjection.appModule
-import org.example.domain.usecase.GetEasyPreparedMealsUseCase
-import org.example.domain.usecase.GetGuessGameUseCase
-import org.example.domain.usecase.GetSweetsWithNoEggsUseCase
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 
@@ -16,19 +11,4 @@ fun main() {
 
     val programConsole: FoodChangeMoodConsole = getKoin().get()
     programConsole.start()
-
-    /*val mealsUseCase = GetEasyPreparedMealsUseCase(MockDataMealRepository())
-    mealsUseCase.getEasyPreparedMeals().forEach {
-        println("name: ${it.name}")
-    }
-    val randomMealUseCase= GetGuessGameUseCase(MockDataMealRepository())
-    val meal = randomMealUseCase.getRandomMeal()
-    val guessedMinutes=20
-    println("name:${randomMealUseCase.getRandomMeal().name}\n" +
-            "correct minutes:${randomMealUseCase.getRandomMeal().minutes} \n" +
-            "guessed minutes:$guessedMinutes ${randomMealUseCase.isGuessCorrectHighOrLow(meal,guessedMinutes)}")
-
-    println(GetSweetsWithNoEggsUseCase(MockDataMealRepository()).getMealHasNoEggs().name)
-    println(GetSweetsWithNoEggsUseCase(MockDataMealRepository()).getMealHasNoEggs().name)
-*/
 }
--- src/main/kotlin/presentation/ConsoleColors.kt
+++ src/main/kotlin/presentation/ConsoleColors.kt
@@ -4,5 +4,5 @@ object ConsoleColors {
     const val RED_COLOR= "\u001B[31m"
     const val MAGENTA_COLOR="\u001B[35m"
     const val GREEN_COLOR= "\u001B[32m"
-    const val RESETCOLOR= "\u001B[0m"
+    const val RESET_COLOR= "\u001B[0m"
 }
