e1ebd31568d4c2069d5752570de25e6d504bc5de
Mohamed <Mohamed.Fwawzy@gmail.com>
2025-05-14 16:50:24 +0300

    enhance in the 2 use cases : GetEasyPreparedMealsUseCase to use random index instead of shuflled GetKetoMealUseCase to not use magic number anymore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,6 @@ import org.example.domain.usecase.GetEasyPreparedMealsUseCase
 fun main() {
     val mealsUseCase = GetEasyPreparedMealsUseCase(MockDataMealRepository())
     mealsUseCase.getEasyPreparedMeals().forEach {
-        println(it.name)
+        println("name: ${it.name}")
     }
 }
