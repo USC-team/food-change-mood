32b8627b5dbabe70f07e1c66bcc7e0f1c544da51
Mohamed <Mohamed.Fwawzy@gmail.com>
2025-05-15 13:11:36 +0300

    solve issue of reading and parsing from food.csv

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 package org.example
 
-import domain.model.Meal
 import org.example.data.repository.MockDataMealRepository
 import org.example.domain.usecase.GetEasyPreparedMealsUseCase
 import org.example.domain.usecase.GetGuessGameUseCase
@@ -9,28 +8,19 @@ import java.io.File
 
 fun main() {
 
-    val mealRepository= MockDataMealRepository()
-    val meals: List<Meal> = mealRepository.loadAll()
-
-    println("Loaded ${meals.size} meals")
-    meals
-        .sortedBy { it.minutes ?: Int.MAX_VALUE }
-        .take(2)
-        .forEach { println(it) }
-
-
-    val mealsUseCase = GetEasyPreparedMealsUseCase(MockDataMealRepository())
+    val file= File("food.csv")
+    val mealsUseCase = GetEasyPreparedMealsUseCase(MockDataMealRepository(file))
     mealsUseCase.getEasyPreparedMeals().forEach {
         println("name: ${it.name}")
     }
-    val randomMealUseCase= GetGuessGameUseCase(MockDataMealRepository())
+    val randomMealUseCase= GetGuessGameUseCase(MockDataMealRepository(file))
     val meal = randomMealUseCase.getRandomMeal()
     val guessedMinutes=20
     println("name:${randomMealUseCase.getRandomMeal().name}\n" +
             "correct minutes:${randomMealUseCase.getRandomMeal().minutes} \n" +
             "guessed minutes:$guessedMinutes ${randomMealUseCase.isGuessCorrectHighOrLow(meal,guessedMinutes)}")
 
-    println(GetSweetsWithNoEggsUseCase(MockDataMealRepository()).getMealHasNoEggs().name)
-    println(GetSweetsWithNoEggsUseCase(MockDataMealRepository()).getMealHasNoEggs().name)
+    println(GetSweetsWithNoEggsUseCase(MockDataMealRepository(file)).getMealHasNoEggs().name)
+    println(GetSweetsWithNoEggsUseCase(MockDataMealRepository(file)).getMealHasNoEggs().name)
 
 }
--- src/main/kotlin/data/repository/CsvParsers.kt
+++ src/main/kotlin/data/repository/CsvParsers.kt
@@ -1,11 +1,41 @@
 package data.repository
 
+import domain.model.Meal
+import domain.model.Nutrition
+
 object CsvParsers {
+
+  fun parseToMeal(row: Map<String, String>): Meal {
+    val nutritionValues = CsvParsers.parseDoubleList(row["nutrition"] ?: "")
+    return Meal(
+      name = row["name"] ?: "",
+      id = row["id"]?.toInt() ?: 0,
+      minutes = row["minutes"]?.toInt(),
+      contributorId = row["contributor_id"]?.toInt(),
+      submitted = row["submitted"] ?: "",
+      tags = CsvParsers.parseStringList(row["tags"] ?: ""),
+      nutrition = Nutrition(
+        calories = nutritionValues.getOrNull(0),
+        totalFat =nutritionValues.getOrNull(1),
+        sugar = nutritionValues.getOrNull(2),
+        sodium = nutritionValues.getOrNull(3),
+        protein = nutritionValues.getOrNull(4),
+        saturatedFat = nutritionValues.getOrNull(5),
+        carbohydrates = nutritionValues.getOrNull(6),
+      ),
+      nSteps = row["n_steps"]?.toInt(),
+      steps = CsvParsers.parseStringList(row["steps"] ?: ""),
+      description = row["description"] ?: "",
+      ingredients = CsvParsers.parseStringList(row["ingredients"] ?: ""),
+      nIngredients = row["n_ingredients"]?.toInt()
+    )
+  }
+
   /**
    * Turns "['a','b','c']" into List("a","b","c"),
    * or `"a,b,c"` (if un-bracketed) into List("a","b","c")
    */
-  fun parseStringList(raw: String): List<String> =
+  private fun parseStringList(raw: String): List<String> =
     raw.trim().let {
       val stripped = it.removePrefix("[").removeSuffix("]")
       // split on commas not inside nested quotes
@@ -18,7 +48,7 @@ object CsvParsers {
   /**
    * Turns "[1.0,2.5,3]" into List(1.0,2.5,3.0)
    */
-  fun parseDoubleList(raw: String): List<Double> =
+  private fun parseDoubleList(raw: String): List<Double> =
     raw.trim().removePrefix("[").removeSuffix("]")
       .split(',')
       .mapNotNull { it.trim().toDoubleOrNull() }
