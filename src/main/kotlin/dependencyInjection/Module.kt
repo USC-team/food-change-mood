0c367e68f78052a611c672b32035d854f200899e
Mohamed <Mohamed.Fwawzy@gmail.com>
2025-05-22 16:30:13 +0300

    add branch for unit test of csvDataRepository

--- src/main/kotlin/dependencyInjection/Module.kt
+++ src/main/kotlin/dependencyInjection/Module.kt
@@ -1,18 +1,15 @@
 package dependencyInjection
 
 
+
 import data.repository.MockDataMealRepository
 import domain.repository.MealsRepository
 import domain.usecase.GetEasyPreparedMealsUseCase
 import domain.usecase.GetGuessGameUseCase
 import domain.usecase.GetKetoMealUseCase
-import data.repository.MockDataMealRepository
-import domain.repository.MealsRepository
-import domain.usecase.GetGuessGameUseCase
 import domain.usecase.GetSweetsWithNoEggsUseCase
 import presentation.FoodChangeMoodConsole
 import org.koin.dsl.module
-import presentation.FoodChangeMoodConsole
 import java.io.File
 
 val appModule = module {
